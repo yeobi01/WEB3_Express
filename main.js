13158072252cc32ad7211b010e519831a7446f52
yeobi01 <yeobi_01@naver.com>
2022-12-10 14:20:15 +0900

    Router 분리

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var fs = require('fs');
 var path = require('path');
 var qs = require('querystring');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var compression = require('compression');
 var sanitizeHtml = require('sanitize-html');
-var compression = require('compression')
 var template = require('./lib/template.js');
+var indexRouter = require('./routes/index');
+var topicRouter = require('./routes/topic');
 const port = 3000
 
 app.use(express.static('public'));
@@ -20,122 +23,9 @@ app.get('*',function(request, response, next){
   });
 });
 
-//app.get('/', (req, res) => { res.send('Hello World!') })
-app.get('/', function(request, response){
-  var title = 'Welcome';
-  var description = 'Hello, Node.js';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list,
-    `
-    <h2>${title}</h2>${description}
-    <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
-    `,
-    `<a href="/create">create</a>`
-  );
-  response.send(html);
-});
+app.use('/',indexRouter);
+app.use('/topic',topicRouter);
 
-app.get('/page/:pageId', function(request, response, next){
-  //var filteredId = path.parse(request.params.pageId).base;
-  var filteredId = request.params.pageId;
-  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-    if(err){
-      next(err);
-    } else {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      response.send(html);
-    }
-  });
-});
-
-app.get('/create',function(request, response){
-  var title = 'WEB - create';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-    <form action="/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>
-  `, '');
-  response.send(html);
-});
-
-app.post('/create_process',function(request, response){
-  var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-    response.writeHead(302, {Location: `/?id=${title}`});
-    response.end();
-    // 뭔가 이상함 고쳐야 하는건 알고 있는데 강의에서 고치지 않아서 두겠음..
-  })
-});
-
-app.get('/update/:pageId',function(request, response){
-  //var filteredId = path.parse(queryData.id).base;
-  var filteredId = request.params.pageId;
-  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = request.params.pageId;
-      var list = template.list(request.list);
-      var html = template.HTML(title, list,
-        `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-      );
-      response.send(html);
-  });
-})
-
-app.post('/update_process',function(request, response){
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function(error){
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.redirect(`/?id=${title}`);
-      // 뭔가 이상함 고쳐야 하는건 알고 있는데 강의에서 고치지 않아서 두겠음..
-    })
-  });
-})
-
-app.post('/delete_process',function(request, response){
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function(error){
-    response.redirect('/');
-  })
-})
 
 app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that!');
